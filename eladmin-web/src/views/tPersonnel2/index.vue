0d59afee5334587c810695204a178fc26081b319
zengjain <506788055@qq.com>
2020-03-26 18:29:51 +0800

    业务受理 前端页面的修改
    Change-Id: I787372aff680776149a850e8d9ce5bf5e27b5984

--- eladmin-web/src/views/tPersonnel2/index.vue
+++ eladmin-web/src/views/tPersonnel2/index.vue
@@ -3,19 +3,18 @@
     <!--工具栏-->
     <div class="head-container">
       <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
-      <crudOperation :permission="permission" />
       <!--表单组件-->
       <!--表单组件-->
-      <el-form ref="form" :model="form" :rules="rules" size="small" inline="true" label-width="200">
-        <el-divider content-position="left">申报人信息</el-divider>
-        <el-form-item label="申报人" prop="tname" style="width: 48%" >
+      <el-form ref="form" :model="form" :rules="rules" size="medium" inline="true" label-width="150px" label-position="left">
+        <el-divider content-position="left" ><el-tag size="medium" effect="plain">申报房产信息</el-tag></el-divider>
+        <el-form-item label="申报人" prop="tname" style="width: 48%"  >
           <el-input v-model="form.tname" style="width: 300px; margin-left: 20px" />
         </el-form-item>
         <el-form-item label="身份证号" prop="cardId" style="width: 48%" >
           <el-input v-model="form.cardId" style="width: 300px; margin-left: 20px;" />
         </el-form-item>
 
-        <el-form-item label="申请人类型" style="width: 48% " prop="sqrlx" >
+        <el-form-item label="申请人类型" style="width: 48% " prop="sqrlx" labelPosition="right">
           <!--<el-radio-group v-model="form.sqrlx" size="mini" style="width: 600px" @change="changeFromRY()">
             <el-radio-button
               v-for="item in dict.peoson_type"
@@ -36,8 +35,8 @@
           <el-input v-model="form.phone" style="width: 300px; margin-left: 20px;" />
         </el-form-item>
 
-        <el-divider  content-position="left">申报房产信息</el-divider>
-        <el-form-item label="房产类型" prop="fclx" style="width:  90%">
+          <el-divider  content-position="left"><el-tag size="medium" effect="plain" >申报房产信息</el-tag></el-divider>
+        <el-form-item label="房产类型" prop="fclx" style="width:  90%" >
           <!--<el-select v-model="form.fclx" filterable placeholder="请选择" @change="changeFromFC()">
               <el-option
                 v-for="item in dict.house_type"
@@ -71,11 +70,11 @@
         <el-form-item  label="增值税发票总金额" prop="fpje" style="width:30%" >
           <el-input v-model="form.btje" style="width: 300px; margin-left: 20px;" />
         </el-form-item>
-        <el-form-item label="不动产权证号（二手房）" prop="fcid" v-if="form.fclx == '标准厂房'" style="width:30%">
-          <el-input v-model="form.btye" style="width: 300px; margin-left: 20px;" />
-        </el-form-item>
         <el-form-item label="购房面积（平方米）" prop="fcmj"  style="width:30%">
         <el-input v-model="form.fcmj" style="width: 300px; margin-left: 20px;" />
+        </el-form-item>
+        <el-form-item label="不动产权证号（二手房）" prop="fcid" v-if="form.fclx == '二手营业用房/自住用房'" label-width="180px" style="width:30%">
+          <el-input v-model="form.btye" style="width: 300px; margin-left: 20px;" />
         </el-form-item>
 
         <!--
@@ -101,9 +100,62 @@
                 <el-form-item label="所处位置">
                   <el-input v-model="form.scwz" style="width: 300px; margin-left: 20px;" />
                 </el-form-item>-->
+        <el-divider  content-position="left"><el-tag size="medium" effect="plain">申报材料</el-tag></el-divider>
+        <el-form-item>
+          <el-table
+            ref="table1"
+            :data="tableData"
+            style="width: 100%"
+            >
+            <el-table-column
+              type="index"
+              label="序号"
+              width="50">
+            </el-table-column>
+            <el-table-column
+              label="资料名称"
+              width="1000"
+              prop="name">
+            </el-table-column>
+            <el-table-column label="操作" width="600px">
+              <template slot-scope="scope">
+                <!--<el-button
+                  size="mini"
+                  type="primary"
+                  plain
+                  @click="handleEdit(scope.$index, scope.row)">上传</el-button>-->
+                <el-upload
+                  class="upload-demo"
+                  action="https://jsonplaceholder.typicode.com/posts/"
+                  multiple
+                  :limit="3"
+                  :on-exceed="handleExceed"
+                  :file-list="fileList">
+                  <el-button
+                    size="mini"
+                    type="primary"
+                    plain> 点击上传 </el-button>
+                </el-upload>
+
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-form-item>
+        <el-dialog
+          title="提示"
+          :visible.sync="centerDialogVisible"
+          width="30%"
+          center>
+          <span>请选择申请人类型</span>
+          <span slot="footer" class="dialog-footer">
+            <el-button @click="centerDialogVisible = false">取 消</el-button>
+            <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
+          </span>
+        </el-dialog>
       </el-form>
-      <!--分页组件-->
-      <pagination />
+
+
+
     </div>
   </div>
 </template>
@@ -131,6 +183,52 @@
           edit: ['admin', 'tShenbaoxingxi:edit'],
           del: ['admin', 'tShenbaoxingxi:del']
         },
+        centerDialogVisible: false,
+        //二手房
+        tableData1: [{
+          name: '申请人身份证明（验原件，收复印件）'
+        }, {
+          name: '增值税普通（或专用）发票（验原件，收复印件）'
+        }, {
+          name: '不动产权证（验原件，收复印件）'
+        }, {
+          name: '农民工务工材料（农民工提供，原件）'
+        }, {
+          name: '学历证明（高校毕业生提供，验原件，收复印件）'
+        }],
+
+        //新建营业房、自住房、一手车位
+        tableData2:[{
+          name: '申请人身份证明（验原件，收复印件）'
+        }, {
+          name: '《商品房买卖合同》（验原件，收复印件）'
+        }, {
+          name: '增值税普通（或专用）发票（验原件，收复印件）'
+        }, {
+          name: '农民工务工材料（农民工提供，原件）'
+        }, {
+          name: '学历证明（高校毕业生提供，验原件，收复印件）'
+        }],
+
+        //标准化厂房
+        tableData3:[{
+          name: '申请人身份证明（验原件，收复印件）'
+        }, {
+          name: '营业执照、公司章程、出资材料（领办企业提供、验原件、收复印件）'
+        }, {
+          name: '不动产属相关资料（验原件，收复印件）'
+        }, {
+          name: '增值税普通（或专用）发票（验原件，收复印件）'
+        }, {
+          name: '农民工务工材料（农民工提供，原件）'
+        }, {
+          name: '学历证明（高校毕业生提供，验原件，收复印件）'
+        }],
+
+        //购买新建商品房且转为城镇户口
+        tableData4:[{
+          name: '申请人身份证明（验原件，收复印件）'
+        }],
         rules: {
           htsj: [
             { required: true, message: '合同时间不能为空', trigger: 'blur' }
@@ -172,16 +270,76 @@
             { required: true, message: '房产面积', trigger: 'blur' }
           ]
         }    }
+
     },
     methods: {
       // 获取数据前设置好接口地址
       [CRUD.HOOK.beforeRefresh]() {
         return true
+      },
+      tableDataOnload(){
+      },
+
+      //改变房产类型触发
+      changeFromFC(){
+        var _this= this;
+        if(!_this.form.sqrlx){
+          _this.centerDialogVisible = true;
+          return;
+        }
+        if (_this.form.sqrlx){
+          if(_this.form.fclx=='标准厂房'){
+            _this.tableData=_this.tableData3;
+              return;
+          }
+          if(_this.form.fclx=='新建营业用房/自住住房'){
+            _this.tableData=_this.tableData2;
+            return;
+          }
+          if(_this.form.fclx=='二手营业用房/自住用房'){
+            _this.tableData=_this.tableData1;
+            return;
+          }
+          if(_this.form.fclx=='一手车位（车库）'){
+            _this.tableData=_this.tableData4;
+            return;
+          }
+        }
+
+      },
+      //改变申请人类型触发
+      changeFromRY(){
+        if(this.form.fclx){
+
+          if(this.form.fclx=='标准厂房'){
+            this.tableData=this.tableData3;
+          }
+          if(this.form.fclx=='新建营业用房/自住住房'){
+            this.tableData=this.tableData2;
+          }
+          if(this.form.fclx=='二手营业用房/自住用房'){
+            this.tableData=this.tableData1;
+          }
+          if(this.form.fclx=='一手车位（车库）'){
+            this.tableData=this.tableData4;
+          }
+        }
+
       }
-    }
-  }
+    },
+
+     }
 </script>
 
 <style scoped>
+  .el-divider__text.is-left {
+    left: 0px;
 
+  }
+  .el-divider__text {
+
+    padding: 0 0px;
+
+
+  }
 </style>
