cbb7daaec74e2ebe16c5f5ed5b8d107ce5003d2b
dengjie <foo4@foo.com>
2020-03-30 11:12:39 +0800

    添加流程的接口
    Change-Id: Ic492ae92647fdd557c5598826fa3435ad71dcf54

---
+++ eladmin-zybt/src/main/java/me/zhengjie/rest/business/LeaveController.java
@@ -0,0 +1,62 @@
+package me.zhengjie.rest.business;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import me.zhengjie.base.XbootBaseController;
+import me.zhengjie.common.utils.ResultUtil;
+import me.zhengjie.common.utils.SecurityUtil;
+import me.zhengjie.common.vo.Result;
+import me.zhengjie.entity.ActBusiness;
+import me.zhengjie.entity.business.Leave;
+import me.zhengjie.service.ActBusinessService;
+import me.zhengjie.service.business.LeaveService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Exrickx
+ */
+@Slf4j
+@RestController
+@Api(description = "请假申请接口")
+@Transactional
+@RequestMapping(value = "/api/leave")
+public class LeaveController extends XbootBaseController<Leave, String> {
+
+    @Autowired
+    private LeaveService leaveService;
+
+    @Autowired
+    private ActBusinessService actBusinessService;
+
+    @Autowired
+    private SecurityUtil securityUtil;
+
+    @Override
+    public LeaveService getService() {
+        return leaveService;
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @ApiOperation(value = "添加申请草稿状态")
+    public Result<Object> add(Leave leave,
+                              @RequestParam String procDefId){
+
+        Leave le = leaveService.save(leave);
+        // 保存至我的申请业务
+        String userId = securityUtil.getCurrUser().getId().toString();
+        ActBusiness actBusiness = new ActBusiness();
+        actBusiness.setUserId(userId);
+        actBusiness.setTableId(le.getId());
+        actBusiness.setProcDefId(procDefId);
+        actBusiness.setTitle(leave.getTitle());
+        actBusinessService.save(actBusiness);
+        return ResultUtil.data(null);
+    }
+}
