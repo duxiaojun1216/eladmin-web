27c2c9bd54ff85855eeaac477a94692d5ab9e77f
yinchaoyin <foo8@foo.com>
2020-03-31 17:35:41 +0800

    删除多余包
    Change-Id: If47dd3117f03a3c3a1aa84e23d363f4ab687e710

--- eladmin-system/src/main/java/me/zhengjie/modules/drools/MessageController.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/drools/MessageController.java
@@ -2,32 +2,20 @@ package me.zhengjie.modules.drools;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import me.zhengjie.aop.log.Log;
 import me.zhengjie.common.utils.ResultUtil;
-import me.zhengjie.common.utils.SecurityUtil;
 import me.zhengjie.common.vo.Result;
-import me.zhengjie.config.activeMq.QueueProducer;
 import me.zhengjie.config.drools.DroolsUtils;
 import me.zhengjie.config.drools.Message;
-import me.zhengjie.system.service.dto.UserDto;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Api(tags = "测试")
 @RestController
@@ -73,31 +61,31 @@ public class MessageController {
 //        return "消息已经发送";
 //    }
 
-    @GetMapping(value = "/testExportExcel", produces = "text/html;charset=UTF-8;")
-    public void testExportExcel(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        String title = "xxxxx测试";
-        String[] rowsName = new String[]{"序号","地区","申报对象类型","标准化厂房","营业用房"
-                ,"自住住房","二手营业用房","二手自住住房","车位","合计","区合计"};
-        List<Object[]> dataList = new ArrayList<Object[]>();
-        Object[] objs = null;
-        for (int i = 0; i < 15; i++) {
-            objs = new Object[rowsName.length];
-            objs[0] = i;
-            objs[1] = "11111";
-            objs[2] = "22222";
-            objs[3] = "33333";
-            objs[4] = "44444";
-            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-            String date = "2020-03-30";
-            objs[5] = date;
-            objs[6] = "44444";
-            objs[7] = "44444";
-            objs[8] = "44444";
-            objs[9] = "44444";
-            objs[10] = "44444";
-            dataList.add(objs);
-        }
-        ExportExcel ex = new ExportExcel(title, rowsName, dataList);
-        ex.exportExcelData(response);
-    }
+//    @GetMapping(value = "/testExportExcel", produces = "text/html;charset=UTF-8;")
+//    public void testExportExcel(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//        String title = "xxxxx测试";
+//        String[] rowsName = new String[]{"序号","地区","申报对象类型","标准化厂房","营业用房"
+//                ,"自住住房","二手营业用房","二手自住住房","车位","合计","区合计"};
+//        List<Object[]> dataList = new ArrayList<Object[]>();
+//        Object[] objs = null;
+//        for (int i = 0; i < 15; i++) {
+//            objs = new Object[rowsName.length];
+//            objs[0] = i;
+//            objs[1] = "11111";
+//            objs[2] = "22222";
+//            objs[3] = "33333";
+//            objs[4] = "44444";
+//            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+//            String date = "2020-03-30";
+//            objs[5] = date;
+//            objs[6] = "44444";
+//            objs[7] = "44444";
+//            objs[8] = "44444";
+//            objs[9] = "44444";
+//            objs[10] = "44444";
+//            dataList.add(objs);
+//        }
+//        ExportExcel ex = new ExportExcel(title, rowsName, dataList);
+//        ex.exportExcelData(response);
+//    }
 }
