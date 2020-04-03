92810f83a70cfbbd19682bd87490af41efbade4d
duxiaojun <243495208@qq.com>
2020-04-03 11:07:48 +0800

    是否
    Change-Id: Ia75000c78a5a90757e53c8a5d7acabf6a634cfbe

--- eladmin-web/src/views/activiti/todo-manage/todoManage.vue
+++ eladmin-web/src/views/activiti/todo-manage/todoManage.vue
@@ -2,86 +2,8 @@
   @import "../../../styles/table-common.less";
   @import "./todoManage.less";
 
-
-  .demo-upload-list{
-    display: inline-block;
-    width: 100px;
-    height: 100px;
-    text-align: center;
-    line-height: 60px;
-    border: 1px solid transparent;
-    border-radius: 4px;
-    overflow: hidden;
-    background: #fff;
-    position: relative;
-    box-shadow: 0 1px 1px rgba(0,0,0,.2);
-    margin-right: 4px;
-  }
-  .demo-upload-list img{
-    width: 100%;
-    height: 100%;
-  }
-  .demo-upload-list-cover{
-    display: none;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background: rgba(0,0,0,.6);
-  }
-  .demo-upload-list:hover .demo-upload-list-cover{
-    display: block;
-  }
-  .demo-upload-list-cover i{
-    color: #fff;
-    font-size: 20px;
-    cursor: pointer;
-    margin: 0 2px;
-  }
-
-
 </style>
 <template>
-
- <!-- <div>
-    <Row>
-      <Col span="6">
-        <Card >
-          <div class="demo-upload-list" v-for="item in file">
-            <img  :src="item.src" />
-            <div class="demo-upload-list-cover">
-              <Icon type="ios-eye-outline" @click="handleView(item)"></Icon>
-              <Icon type="ios-trash-outline" @click="handleRemove(item)"></Icon>
-            </div>
-          </div>
-        </Card>
-      </Col>
-
-      <Col span="12" style="margin-top: 100px;">
-        <Upload
-          multiple
-          :before-upload="handleUpload"
-          :action = action
-          :format="['jpg','jpeg','png']"
-          :on-format-error="uploadFormatError"
-        >
-          <Button icon="ios-cloud-upload-outline">上传附件</Button>
-        </Upload>
-        <Button type="success" style="width: 100px;" @click="upload()">提交</Button>
-      </Col>
-    </Row>
-
-
-    <Modal title="View Image" width="50" v-model="viewModal" footer-hide>
-      <div>
-        <img :src="src" style="width: 100%"/>
-      </div>
-    </Modal>
-
-  </div>-->
-
-
   <div class="search">
     <Card>
       <Row v-show="openSearch" @keydown.enter.native="handleSearch">
@@ -174,45 +96,7 @@
           <Input type="textarea" v-model="form.comment" :rows="4"/>
         </FormItem>
 
-       <!-- <Col span="6">
-          <Card >
-            <div class="demo-upload-list" v-for="item in file">
-              <img  :src="item.src" />
-              <div class="demo-upload-list-cover">
-                <Icon type="ios-eye-outline" @click="handleView(item)"></Icon>
-                <Icon type="ios-trash-outline" @click="handleRemove(item)"></Icon>
-              </div>
-            </div>
-          </Card>
-        </Col>
 
-        <Col span="12" style="margin-top: 100px;">
-          <Upload
-            multiple
-            :before-upload="handleUpload"
-            :action = action
-            :format="['jpg','jpeg','png']"
-            :on-format-error="uploadFormatError"
-          >
-            <Button icon="ios-cloud-upload-outline">上传附件</Button>
-          </Upload>
-          <Button type="success" style="width: 100px;" @click="upload()">提交</Button>
-        </Col>-->
-        <!--<FormItem label="下一审批人" prop="assignees" v-show="showAssign" :error="error">
-          <Select
-            v-model="form.assignees"
-            placeholder="请选择或输入搜索"
-            filterable
-            clearable
-            multiple
-            :loading="userLoading"
-          >
-            <Option v-for="(item, i) in assigneeList" :key="i" :value="item.id">{{item.username}}</Option>
-          </Select>
-        </FormItem>
-        <FormItem label="下一审批人" v-show="isGateway">
-          <span>分支网关处暂不支持自定义选择下一审批人，将发送给下一节点所有人</span>
-        </FormItem>-->
         <template>
           <Upload
             class="upload-demo"
@@ -222,30 +106,9 @@
             :on-success="uploadSuccess"
             :on-error="uploadError">
             <Button icon="ios-cloud-upload-outline">上传附件</Button>
-            <!--<el-button
-              icon="ios-cloud-upload-outline"
-              size="mini"
-              type="primary"
-              plain>
-              上传附件
-            </el-button>-->
-            <!--<el-button
-
-              size="mini"
-              type="primary"
-              plain> 预览 </el-button>-->
             <el-button> 预览 </el-button>
           </Upload>
         </template>
-        <!--<Upload
-                     multiple
-                    :before-upload="handleUpload"
-                    :action = action
-                    :format="['jpg','jpeg','png']"
-                    :on-format-error="uploadFormatError">
-                    <Button icon="ios-cloud-upload-outline">上传附件</Button>
-                </Upload>
-                -->
         <div v-show="form.type==1">
           <FormItem label="驳回至">
             <Select
@@ -313,114 +176,6 @@
   } from '@/api/activiti';
   import request from '@/utils/request'
   import { searchUserByName, getDictDataByType } from '@/api/index';
-
-  /*export default {
-
-    data() {
-      return {
-        // 预览图片的src
-        src:'',
-        // 预览图片Modal
-        viewModal:false,
-        // 随便写个地址
-        action: 'notice/run',
-        // 上传文件
-        file: [],
-      }
-    },
-
-    created() {
-
-    },
-
-    mounted() {
-
-    },
-
-    methods: {
-
-      /!**
-       *  方法作用:  去除上传文件
-       *  时间: 2018年12月12日 18:51:38
-       *  创建人:  Cr
-       **!/
-      handleRemove (file) {
-        this.file.splice(this.file.indexOf(file), 1);
-      },
-
-      /!**
-       *  方法作用:  上传类型错误提示
-       *  时间: 2018年12月12日 18:51:38
-       *  创建人:  Cr
-       **!/
-      uploadFormatError(){
-        this.$Message.error('上传类型只能是jpg,jpeg,png');
-      },
-
-      /!**
-       *  方法作用:  预览图片
-       *  时间: 2018年12月12日 18:51:38
-       *  创建人:  Cr
-       **!/
-      handleView (item) {
-        this.src = item.src;
-        this.viewModal = true;
-      },
-
-      /!**
-       *  方法作用:  获取上传文件,并去掉重复的文件
-       *  时间: 2018年11月26日 18:38:13
-       *  创建人:  Cr
-       **!/
-      handleUpload (file) {
-        file.src = this.convertSrc(file);
-        this.file.push(file);
-        // false代表不上传到action的地址，true的话会报错，因为action地址是瞎写的，action地址不写会报错。
-        return false;
-      },
-
-      /!**
-       *  方法作用:  上传
-       *  时间: 2018年11月26日 18:38:13
-       *  创建人:  Cr
-       **!/
-      upload(){
-        let that = this;
-
-        if(that.file.length == 0){
-          that.$Message.error('上传文件不能为空');
-          return;
-        }
-
-        let formData = new FormData();
-
-        for(let i = 0; i < that.file.length; i++) {
-          formData.append('file', that.file[i]);
-        }
-
-        let config = {
-          headers: {
-            'Content-Type': 'multipart/form-data;boundary = ' + new Date().getTime()
-          }
-        };
-
-        /!* that.$ajax.post('/upload',formData, config).then(response => {
-         });*!/
-
-      },
-
-      /!**
-       *  方法作用:  转换成SRC显示在页面上
-       *  时间: 2018年11月26日 18:38:13
-       *  创建人:  Cr
-       **!/
-      convertSrc(file){
-        return window.URL.createObjectURL(file);
-      },
-
-    },
-  }*/
-
 
   export default {
     name: 'todo-manage',
@@ -749,7 +504,8 @@
         data: [], // 表单数据
         total: 0, // 表单数据总数
         dictPriority: [{ title: '测试', value: '0' }],
-        isGateway: false
+        isGateway: false,
+        region: null
       };
     },
 
@@ -888,22 +644,7 @@
           url: '/api/leave/getPer',
           method: 'get'
         }).then(function(res) {
-          //console.debug(res)
-          if (res == 'gaj') {
-            _this.isborder = true;   //农民
-          }else if(res == 'zjbm'){
-            _this.isgraduate = true;
-            _this.isforeign = true;
-          }else if(res == 'rs'){
-            _this.isborder = true;
-          }else if(res == 'ty'){
-            _this.issoldier = true;
-          }
-          /*isborder: false,
-            issoldier: false,
-            isgraduate: false,
-            isforeign: false,*/
-          console.debug(_this.isgraduate)
+          _this.region =res;
         });
       },
       getDictDataType() {
@@ -1067,6 +808,27 @@
         });
       },
       passTask(v) {
+        if (v.variables.bh.substring(0, 2) == 'nm') {
+          this.isborder = true;   //农民
+          this.isgraduate =false;
+          this.isforeign = false;
+          this.issoldier =false;
+        }else if(v.variables.bh.substring(0, 2) == 'gx') {
+          this.isgraduate = true;  //高校
+          this.isborder = false;
+          this.isforeign = false;
+          this.issoldier = false;
+        }else if (v.variables.bh.substring(0, 2) == 'wl'){
+          this.isforeign = true;  //外来
+          this.isborder = false;
+          this.isgraduate =false;
+          this.issoldier =false;
+        }else if (v.variables.bh.substring(0, 2) == 'ty'){
+          this.issoldier= true; //退役
+          this.isborder = false;   //农民
+          this.isgraduate =false;
+          this.isforeign = false;
+        }
         this.modalTaskTitle = '审批通过';
         this.form.id = v.id;
         this.form.procInstId = v.procInstId;
